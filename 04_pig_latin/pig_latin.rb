99465c87215c744de3e3e661026721a9e1d8e74a
Catherine <catvo91@gmail.com>
2016-12-07 17:07:56 -0800

    first 120716

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,5 +1,15 @@
 def translate(s)
-	if s[0] = 'a'
+	if s[0] == ('a' || 'e' || 'i' || 'o' || 'u')
 		s + 'ay'
+	else
+		if s[1] == ('a' || 'e' || 'i' || 'o' || 'u') #if the second letter is a vowel
+			firstLetters = s.slice!(0)
+			s + firstLetters + 'ay'
+		else #if the second letter is consenant 
+			firstLetters = s.slice!(0..1)
+			s + firstLetters + 'ay'
+		end
 	end
 end
+
+#cherry 
--- 04_pig_latin/test.rb
+++ 04_pig_latin/test.rb
@@ -1,8 +1,36 @@
-s = 'apple'
 
-cut = s.slice!(0)
-puts s
-puts s + cut
 
-s = 'apple'
-puts s + 'ay'
+def translate(s)
+	s = s.split
+	puts s.to_s
+	puts s.length
+	answer = ''
+	i = 0
+	while i < s.length 
+		puts i.to_s + ' this is i going into the while loop'
+	if s[i][0] == ('a' || 'e' || 'i' || 'o' || 'u')
+		puts s[i][0]
+		answer += s[i] + 'ay' + ' ' #this is the problem, s is an array now
+		puts answer
+	else
+		if s[i][1] == ('a' || 'e' || 'i' || 'o' || 'u') #if the second letter is a vowel (ie only one starting consenant)
+			firstLetters = s[i].slice!(0)
+			puts firstLetters + ' first letters'
+			answer += s[i] + firstLetters + 'ay' + ' '
+		else
+			firstLetters = s[i].slice!(0..1) # if the second letter is a consenent
+			answer += s[i] + firstLetters + 'ay' + ' ' # problem here
+			puts i.to_s + ' in this else loop' # so it says i is not an integer here
+			puts s[i][1] + ' is the second letter'
+			puts firstLetters + ' first letters 2'
+			puts answer
+		end
+	end
+		i += 1
+		puts i.to_s + ' this is i going out of the loop'
+	end
+end
+
+translate('apple pie')
+#            0    1
+# now outputting appleay epiay
