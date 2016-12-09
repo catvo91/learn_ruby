299c5145e7c79a3533349f4e8842a7d7e02ded5a
Catherine <catvo91@gmail.com>
2016-12-08 17:26:56 -0800

    first 120816

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,15 +1,23 @@
 def translate(s)
-	if s[0] == ('a' || 'e' || 'i' || 'o' || 'u')
-		s + 'ay'
+	s = s.split
+	answer = ''
+	i = 0
+	while i < s.length 
+	if s[i][0] == 'a' || s[i][0] == 'e' || s[i][0] == 'i' || s[i][0] == 'o' || s[i][0] == 'u'
+		answer += s[i] + 'ay' + ' ' #this is the problem, s is an array now
+		puts answer
 	else
-		if s[1] == ('a' || 'e' || 'i' || 'o' || 'u') #if the second letter is a vowel
-			firstLetters = s.slice!(0)
-			s + firstLetters + 'ay'
-		else #if the second letter is consenant 
-			firstLetters = s.slice!(0..1)
-			s + firstLetters + 'ay'
+		if s[i][1] == 'a' || s[i][1] == 'e' || s[i][1] == 'i' || s[i][1] == 'o' || s[i][1] == 'u' #if the second letter is a vowel (ie only one starting consenant)
+			firstLetters = s[i].slice!(0)
+			answer += s[i] + firstLetters + 'ay' + ' '
+			puts answer
+		else
+			firstLetters = s[i].slice!(0..1) # if the second letter is a consenent
+			answer += s[i] + firstLetters + 'ay' + ' ' # problem here
+			puts answer
 		end
 	end
+		i += 1
+	end
+	answer = answer.rstrip()
 end
-
-#cherry 
--- 04_pig_latin/test.rb
+++ 04_pig_latin/test.rb
@@ -1,36 +1,31 @@
-
-
 def translate(s)
 	s = s.split
-	puts s.to_s
-	puts s.length
 	answer = ''
 	i = 0
 	while i < s.length 
-		puts i.to_s + ' this is i going into the while loop'
-	if s[i][0] == ('a' || 'e' || 'i' || 'o' || 'u')
-		puts s[i][0]
+	if s[i][0] == 'a' || s[i][0] == 'e' || s[i][0] == 'i' || s[i][0] == 'o' || s[i][0] == 'u'
 		answer += s[i] + 'ay' + ' ' #this is the problem, s is an array now
 		puts answer
-	else
-		if s[i][1] == ('a' || 'e' || 'i' || 'o' || 'u') #if the second letter is a vowel (ie only one starting consenant)
+	else #should refactor this to say, if the first letter is a consenent, check if the second is one, than the third, and perhaps use .join with arrays
+		if s[i][1] == 'a' || s[i][1] == 'e' || s[i][1] == 'i' || s[i][1] == 'o' || s[i][1] == 'u' #if the second letter is a vowel (ie only one starting consenant)
 			firstLetters = s[i].slice!(0)
-			puts firstLetters + ' first letters'
 			answer += s[i] + firstLetters + 'ay' + ' '
+			puts answer
 		else
 			firstLetters = s[i].slice!(0..1) # if the second letter is a consenent
 			answer += s[i] + firstLetters + 'ay' + ' ' # problem here
-			puts i.to_s + ' in this else loop' # so it says i is not an integer here
-			puts s[i][1] + ' is the second letter'
-			puts firstLetters + ' first letters 2'
 			puts answer
 		end
 	end
 		i += 1
-		puts i.to_s + ' this is i going out of the loop'
 	end
+	answer = answer.rstrip()
 end
 
-translate('apple pie')
-#            0    1
+puts translate('apple')
+puts translate('banana')
+puts translate('cherry')
+puts translate('eat pie')
+puts translate('three')
+#            1    1
 # now outputting appleay epiay
